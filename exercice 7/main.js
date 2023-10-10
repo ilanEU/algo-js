d71858cd0aa0f8146e2fecf8fe289076ef5b81bb
ilanEU <147376940+ilanEU@users.noreply.github.com>
2023-10-10 12:46:37 +0200

    Add files via upload

---
+++ exercice 7/main.js
@@ -0,0 +1,12 @@
+let names = []
+console.log(names);
+
+names.push("Vincent", "Paul", "Arthur")
+console.log(names); 
+
+ names.forEach(name => {
+  name += "va à la pêche"
+  console.log(name);
+ });
+
+ 
