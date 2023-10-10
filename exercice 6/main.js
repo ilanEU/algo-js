68ccd389e1e8c67dc9544718ed4c9e32067f8820
ilanEU <147376940+ilanEU@users.noreply.github.com>
2023-10-10 12:51:26 +0200

    ajout exercice 6

---
+++ exercice 6/main.js
@@ -0,0 +1,15 @@
+let a = "Jean"
+let b = "Paul"
+
+
+function checkname(name1, name2) {
+  if (name1 == name2) {
+    return true;
+  } else {
+    return false;
+  
+}
+}
+
+let result =  checkname(a , b)
+console.log(result);
